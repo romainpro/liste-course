f8f6e204e845be8ad1071b7f556532e815d72861
romainpro <romain.casagrande.pro@gmail.com>
2023-06-13 14:21:05 +0200

    format-js

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 const btnAdd = document.getElementById(`add`);
-const ul=document.getElementById(`ul`)
+const ul = document.getElementById(`ul`);
 
-btnAdd.addEventListener(`click`,addText)
+btnAdd.addEventListener(`click`, addText);
 
-
-
-function addText(){
-    let data = document.getElementById(`inputText`).value
-     if(data !==(``)){
-        console.log(data)
-        let li = document.createElement(`li`)
-        li.textContent=data
-        ul.appendChild(li)
-
-     }else{
-        console.log(`rien`)
-     }
-     
-
+function addText() {
+  let data = document.getElementById(`inputText`).value;
+  if (data !== ``) {
+    console.log(data);
+    let li = document.createElement(`li`);
+    li.textContent = data;
+    ul.appendChild(li);
+  } else {
+    console.log(`rien`);
+  }
 }
